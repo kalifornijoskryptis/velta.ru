8551c2eacbf956a1ce7a7cc3959be940f59a73dd
feeria <hi@feeria.lt>
2014-07-31 17:09:54 +0300

    Testing: geolocation features
    Geolocation added.

--- html/js/map.js
+++ html/js/map.js
@@ -1,11 +1,20 @@
 function init(){
 		
 		/* Инициализация карты Яндекс и настройки отображения интерфейса */
-		var mapOverview = new ymaps.Map("ymaps-overview", {
-		center: [55.76, 37.64], 
-		zoom: 8,
-		controls: ["routeEditor","geolocationControl","trafficControl", "fullscreenControl", "zoomControl"]
-		});
+		var geolocation = ymaps.geolocation,
+			mapOverview = new ymaps.Map("ymaps-overview", {
+				center: [55.76, 37.64], 
+				zoom: 8,
+				controls: ["routeEditor","geolocationControl","trafficControl", "fullscreenControl", "zoomControl"]
+			});
+		
+		geolocation.get({
+					provider: 'browser',
+					mapStateAutoApply: true
+				}).then(function (result) {
+					//mapOverview.geoObjects.add(result.geoObjects);
+					coord = result.geoObjects.get(0).geometry.getCoordinates();
+				});
 		
 		/* Параметры меток */
 		pmSokol = new ymaps.Placemark([55.77372, 37.738045], {}, {
@@ -51,6 +60,22 @@ function init(){
            }
 		);
 		
+		$("a.geolocate-car-sokol").click( function()
+           {
+			var SokolCoords = pmSokol.geometry.getCoordinates();
+			ymaps.route([
+				[coord], [SokolCoords]]).then(function (route) {
+				route.getPaths().options.set({
+					strokeColor: 'a8ff1e', opacity: 0.9
+				});
+			mapOverview.geoObjects.add(route);
+			var points = route.getWayPoints(),
+            lastPoint = points.getLength() - 1;
+				points.options.set('preset', 'islands#geolocationIcon');
+				points.get(lastPoint).options.set('preset', 'islands#oliveCircleDotIcon');
+			});
+			});
+		
 		/* Клик по вкладке салона в ТЦ "Снегири" */
 		$("a.tab3").click( function()
            {
@@ -58,5 +83,22 @@ function init(){
              mapOverview.container.fitToViewport();
            }
 		);
+		
+		$("a.geolocate-car-sneg").click( function()
+           {
+			var SnegCoords = pmSneg.geometry.getCoordinates();
+			ymaps.route([
+				[coord], [SnegCoords]]).then(function (route) {
+				route.getPaths().options.set({
+					strokeColor: 'a8ff1e', opacity: 0.9
+				});
+			mapOverview.geoObjects.add(route);
+			var points = route.getWayPoints(),
+            lastPoint = points.getLength() - 1;
+				points.options.set('preset', 'islands#geolocationIcon');
+				points.get(lastPoint).options.set('preset', 'islands#oliveCircleDotIcon');
+			});
+			});
+
 }
 ymaps.ready(init);
